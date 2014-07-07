82b1e1c0b031f8be6d4f5c9f09dc99be1dbe027a
tttodorov <tttodorov13@gmail.com>
2014-07-07 17:46:53 +0300

    change year and version to 2.1

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
 <footer id="footer">
     <div class="container_8 clearfix" id="footer-inner">
         <div class="ac grid_8">
-            <span>Copyright <sup>©</sup> <?php echo UCICT; ?> <?php echo SIDE_TITLE; ?> 2013 v2.0</span>
+            <span>Copyright <sup>©</sup> <?php echo UCICT; ?> <?php echo SIDE_TITLE; ?> 2014 v2.1</span>
         </div>
     </div>
 </footer>
